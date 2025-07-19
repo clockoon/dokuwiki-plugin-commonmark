d4a82db50bd98b50105b3a9ef73b66c59dd22fe5
Sungbin Jeon <clockoon@gmail.com>
2025-07-19 17:28:44 +0000

    add config option

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -7,3 +7,4 @@
  */
 $lang['force_commonmark'] = 'Commonmark as default parser, without doctype (use with caution!)';
 $lang['frontmatter_tag'] = 'Set whether parsing tag/tags node in frontmatter, and its location (NOTE: you must enable tag plugin first)';
+$lang['fix_secedit'] = 'Fix section edit position matches into actual Commonmark document (NOTE: you must clear cache by doing "?purge=true")';
--- lang/ko/settings.php
+++ lang/ko/settings.php
@@ -7,3 +7,4 @@
  */
 $lang['force_commonmark'] = '모든 문서를 Commonmark로 처리. Doctype을 입력하지 않아도 됩니다(사용에 유의하세요!).';
 $lang['frontmatter_tag'] = "Frontmatter에 작성한 tag 노드를 파싱할 것인지, 렌더링 위치가 어디인지(최상단/최하단) 설정합니다(주의: tag 플러그인을 먼저 활성화해야 합니다).";
+$lang['fix_secedit'] = 'Commonmark 문서에 맞게 섹션 편집 위치 조정(주의: "?purge=true" 등을 통해 캐시를 제거해야 합니다),';
